b87c69ed7798ff43aa90d8689b2345f084f54adb
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-06-07 15:00:00 +0300

    impl: IsFalseOrTrueUsingAnd function

--- src/main/java/hei/school/calculus_ratiocinator/Statement.java
+++ src/main/java/hei/school/calculus_ratiocinator/Statement.java
@@ -10,12 +10,20 @@ import lombok.NoArgsConstructor;
 public class Statement {
     private String body;
     private String status;
-    public static String IsFalseOrTrue(Statement statement1, Statement statement2){
+    //conjunctions thus and or
+    public static String IsFalseOrTrueUsingThusOr(Statement statement1, Statement statement2){
         if (statement1.getStatus().equals("TRUE") && statement2.getStatus().equals("TRUE") || (statement1.getStatus().equals("TRUE") || statement2.getStatus().equals("TRUE"))){
             return "TRUE";
         }else if((statement1.getStatus().equals("FALSE") && statement2.getStatus().equals("FALSE"))) {
             return "FALSE";
         }
         return "UNKNOWN";
+    }
+
+    public static String IsFalseOrTrueUsingAnd(Statement statement1, Statement statement2){
+        if (statement1.getStatus().equals("FALSE") || statement2.getStatus().equals("FALSE")){
+            return "FALSE";
+        }
+        return "TRUE";
     }
 }
--- src/test/java/hei/school/calculus_ratiocinator/StatementTest.java
+++ src/test/java/hei/school/calculus_ratiocinator/StatementTest.java
@@ -9,27 +9,27 @@ class StatementTest {
     void testFalse() {
         Statement statement1 = new Statement("Lou est beau", "FALSE");
         Statement statement2 = new Statement("Lou est pauvre", "FALSE");
-        assertEquals("FALSE", Statement.IsFalseOrTrue(statement1, statement2));
+        assertEquals("FALSE", Statement.IsFalseOrTrueUsingThusOr(statement1, statement2));
     }
 
     @Test
     void testTrue(){
         Statement statement3 = new Statement("Lou est généreux", "TRUE");
         Statement statement4 = new Statement("Lou est beau", "TRUE");
-        assertEquals("TRUE", Statement.IsFalseOrTrue(statement3, statement4));
+        assertEquals("TRUE", Statement.IsFalseOrTrueUsingThusOr(statement3, statement4));
     }
 
     @Test
     void testTrueAndFalse(){
         Statement statement5 = new Statement("Lou est généreux", "TRUE");
         Statement statement6 = new Statement("Lou est pauvre", "FALSE");
-        assertEquals("TRUE", Statement.IsFalseOrTrue(statement5, statement6));
+        assertEquals("TRUE", Statement.IsFalseOrTrueUsingThusOr(statement5, statement6));
     }
 
     @Test
     void testFalseAndTrue(){
-        Statement statement7 = new Statement("Lou est pauvre", "FALSE");
-        Statement statement8 = new Statement("Lou est généreux", "TRUE");
-        assertEquals("TRUE", Statement.IsFalseOrTrue(statement7, statement8));
+        Statement statement7 = new Statement("Lou est généreux", "TRUE");
+        Statement statement8 = new Statement("Lou est pauvre", "FALSE");
+        assertEquals("FALSE", Statement.IsFalseOrTrueUsingAnd(statement7, statement8));
     }
 }
